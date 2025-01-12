de95c7ffacd2589bf10ab97b28d01ed97d37d81f
aopalka <79727669+aopalka@users.noreply.github.com>
2025-01-12 10:57:09 -0500

    Simplified COTS
    Removed the dependencies from main example project.

--- Examples/SimpleSwerve/src/main/java/frc/robot/RobotContainer.java
+++ Examples/SimpleSwerve/src/main/java/frc/robot/RobotContainer.java
@@ -12,14 +12,12 @@ import BobcatLib.Hardware.Controllers.OI;
 import BobcatLib.Subsystems.Swerve.SimpleSwerve.SwerveDrive;
 import BobcatLib.Subsystems.Swerve.SimpleSwerve.Commands.ControlledSwerve;
 import BobcatLib.Subsystems.Swerve.SimpleSwerve.Commands.TeleopSwerve;
-import BobcatLib.Subsystems.Swerve.Utility.CotsModuleSwerveConstants;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import frc.robot.UnifiedModuleConfigurator.CotsModuleObject;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
