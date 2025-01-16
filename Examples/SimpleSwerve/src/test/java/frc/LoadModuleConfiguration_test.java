e35f46d2cde9af9a964069fb96fa8035acb71144
AO <aopalka@protonmail.com>
2025-01-16 09:29:48 -0500

    Fix dependencies

--- Examples/SimpleSwerve/src/test/java/frc/LoadModuleConfiguration_test.java
+++ Examples/SimpleSwerve/src/test/java/frc/LoadModuleConfiguration_test.java
@@ -8,8 +8,8 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
-import BobcatLib.Subsystems.Swerve.Utility.CotsModuleSwerveConstants;
 import BobcatLib.Subsystems.Swerve.Utility.*;
+import BobcatLib.Subsystems.Swerve.Utility.UnifiedModuleConfigurator.CotsModuleObject;
 
 public class LoadModuleConfiguration_test {
     /**
