a6a9ccb07eeb76523d9b58f38c715cada3423487
aopalka <79727669+aopalka@users.noreply.github.com>
2025-01-21 07:31:07 -0500

    2025.2.5
    weird bracket thing! Not sure why it is auto inserting {} in my version vendordep

--- Examples/Vision/ExampleVision/src/main/java/frc/robot/RobotContainer.java
+++ Examples/Vision/ExampleVision/src/main/java/frc/robot/RobotContainer.java
@@ -31,6 +31,9 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import BobcatLib.Subsystems.Vision.Limelight.LimelightCamera;
+
+
 /**
  * This class is where the bulk of the robot should be declared. Since
  * Command-based is a
@@ -82,9 +85,6 @@ public class RobotContainer {
         
                         // Configure the button bindings
                         configureButtonBindings(); 
-                        
-        
-                        limelightVision = new  limelight();
                 }
         
                 public void initComand() {
@@ -104,6 +104,12 @@ public class RobotContainer {
                                                         s_Controls.controllerJson));
         
                 }
+
+                public void periodic(){
+                        limelightVision.updatePoseEstimator(s_Swerve.getGyroYaw(),s_Swerve.getModulePositions());
+                        limelightVision.periodic();
+                }
+
         
                 public boolean autoChooserInitialized() {
                         return autoChooser.get() != null;
